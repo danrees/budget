4d8f4d95315f1413a9a0c667f76cc6b87d224a67
Daniel Rees <daniel_james_rees@hotmail.com>
2019-12-29 14:11:55 -0500

    works with burntsushi csv serde

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
+//Notes:
+//Create a struct to represent a row
+//row to vector of values? vector of enum type for a row, transaction?
+
 use std::path::Path;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
 use std::error::Error;
+use serde::Deserialize;
+use std::process;
+
+#[derive(Debug,Deserialize)]
+struct Transaction {
+    #[serde(rename = "Date")]
+    date: String,
+    #[serde(rename = " Transaction Details")]
+    transaction_details: String,
+    #[serde(rename = " Funds Out")]
+    funds_out:Option<f64>,
+    #[serde(rename = " Funds In")]
+    funds_in: Option<f64>,
+}
+
+fn print_me(path: &Path) -> Result<(),Box< Error>> {
+    let mut rdr = csv::Reader::from_path(path)?;
+    for result in rdr.deserialize() {
+        let record: Transaction = result?;
+        println!("{:?}",record)
+    }
+    Ok(())
+}
 
 fn main() {
     let path = Path::new("/home/dan/financial/simplii/SIMPLII.csv");
-    let display = path.display();
 
-    let file = match File::open(&path) {
-        Err(why) => panic!("could not open {}: {}", display, why.description()),
-        Ok(file) => file,
-    };
+        if let Err(err) = print_me(path) {
+            println!("error running: {}", err);
+            process::exit(1);
+        }
 
-    let f = BufReader::new(&file);
-
-    for line in f.lines() {
-        let l = line.unwrap();
-        println!("{}",l)
-    }
 }
